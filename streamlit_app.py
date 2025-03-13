4341d73ea74a06de7379e00d13581628cdb5de4f
MC <146663985+mchour11@users.noreply.github.com>
2025-03-13 14:58:36 +0000

    update styling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+# Style font
+with open("style.css") as css:
+    st.markdown( f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
 # Charger les données
 file_path = "250310_EDN_SDD.csv"
 df = pd.read_csv(file_path, sep=None, engine="python")
 
 # Interface Streamlit
-st.title("Explorateur EDN/ECOS — ECOS Alpha")
+st.title("Explorateur EDN/ECOS")
 
 # Ajouter une colonne combinée pour affichage
 df["SDD_display"] = df["SDD_num"].astype(str) + " - " + df["SDD"]
@@ -17,15 +21,20 @@ sdd_sorted = df[["SDD_num", "SDD_display"]].drop_duplicates().sort_values("SDD_n
 edn_sorted = df[["EDN_num", "EDN_display"]].drop_duplicates().sort_values("EDN_num")
 
 # Choix du mode d'exploration
-mode = st.radio("Choisissez un mode :", ["Par SDD", "Par EDN"])
+mode = st.radio("Choisissez un mode :", ["Par SDD", "Par item EDN"])
 
 if mode == "Par SDD":
-    sdd_choice = st.selectbox("Sélectionnez une Situation de Départ (SDD) :", sdd_sorted.set_index("SDD_num")["SDD_display"])
+    sdd_choice = st.selectbox("Sélectionnez une situation de départ (SDD) :", sdd_sorted.set_index("SDD_num")["SDD_display"])
     result = df[df["SDD_display"] == sdd_choice][["EDN_num", "EDN"]].drop_duplicates()
-    st.write(f"### Items EDN liés à la SDD {sdd_choice} :")
-    st.dataframe(result.set_index("EDN_num"))
+    result.columns = ["Numéro d'item", "Nom d'item"]  # Rename the columns
+    result = result.sort_values("Numéro d'item", ascending=True)  # Sort in descending order
+    st.write(f"### Items EDN liés à la SDD <i>{sdd_choice}</i> :", unsafe_allow_html=True)
+    #st.write(f"### Items EDN liés à la SDD {sdd_choice} :")
+    st.dataframe(result.set_index("Numéro d'item"))
 else:
     edn_choice = st.selectbox("Sélectionnez un item EDN :", edn_sorted.set_index("EDN_num")["EDN_display"])
     result = df[df["EDN_display"] == edn_choice][["SDD_num", "SDD"]].drop_duplicates()
-    st.write(f"### Situations de Départ liées à l'item EDN {edn_choice} :")
-    st.dataframe(result.set_index("SDD_num"))
+    result.columns = ["Numéro de SDD", "Nom de SDD"]  # Rename the columns
+    result = result.sort_values("Numéro de SDD", ascending=True)  # Sort in descending order
+    st.write(f"### SDD liées à l'item EDN {edn_choice} :")
+    st.dataframe(result.set_index("Numéro de SDD"))
